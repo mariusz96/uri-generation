dc91b3b6c7a19727bf2a77fa0b2d394a3bd2a0c6
Mariusz Stępień <mariopl@poczta.onet.eu>
2022-08-16 01:01:44 +0200

    fix formatting

--- src/UriGeneration/UriOptions.cs
+++ src/UriGeneration/UriOptions.cs
@@ -4,12 +4,11 @@ namespace UriGeneration
 {
     public class UriOptions : LinkOptions
     {
-        public static UriOptions Default { get; } =
-            new()
-            {
-                BypassCachedExpressionCompiler = false,
-                BypassMethodCache = false
-            };
+        public static UriOptions Default { get; } = new()
+        {
+            BypassCachedExpressionCompiler = false,
+            BypassMethodCache = false
+        };
 
         public bool BypassCachedExpressionCompiler { get; set; }
         public bool BypassMethodCache { get; set; }
