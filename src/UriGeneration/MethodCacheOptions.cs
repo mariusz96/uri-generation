aa159e67a486f9157ac8717891ace7f6f2cd9cd6
Mariusz Stępień <mariopl@poczta.onet.eu>
2022-08-19 00:11:25 +0200

    cannot use null sizelimit after all

--- src/UriGeneration/MethodCacheOptions.cs
+++ src/UriGeneration/MethodCacheOptions.cs
@@ -2,7 +2,7 @@
 {
     public class MethodCacheOptions
     {
-        public long? SizeLimit { get; set; }
+        public long SizeLimit { get; set; }
         public double CompactionPercentage { get; set; }
     }
 }
