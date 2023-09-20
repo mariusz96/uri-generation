686a555d9c993db4f6d2f753d2f2f44e2927d71a
Mariusz Stępień <mariopl@poczta.onet.eu>
2023-09-21 00:00:24 +0200

    use null guards only in public apis

--- src/UriGeneration/Internal/UriGenerator.cs
+++ src/UriGeneration/Internal/UriGenerator.cs
@@ -248,11 +248,6 @@ namespace UriGeneration.Internal
             UriOptions? options = null)
                 where TController : class
         {
-            if (action == null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-
             if (!_valuesExtractor.TryExtractValues<TController>(
                 action,
                 out var values,
