ad0e5fe2c4e4f019efaee1af2afe28fa4856309d
Mariusz Stępień <mariopl@poczta.onet.eu>
2024-11-13 20:55:23 +0100

    remove partial class

--- src/UriGeneration/Internal/MethodCacheEntry.cs
+++ src/UriGeneration/Internal/MethodCacheEntry.cs
@@ -3,25 +3,35 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace UriGeneration.Internal
 {
-    internal partial class MethodCacheEntry
+    internal class MethodCacheEntry
     {
+        private static readonly MethodCacheEntry InvalidInstance =
+            new(
+                isValid: false,
+                actionDescriptor: null);
+
         [MemberNotNullWhen(
             true,
             nameof(ActionDescriptor))]
         public bool IsValid { get; }
         public ControllerActionDescriptor? ActionDescriptor { get; }
 
-        private MethodCacheEntry(bool isValid)
-        {
-            IsValid = isValid;
-        }
-
         private MethodCacheEntry(
             bool isValid,
-            ControllerActionDescriptor actionDescriptor)
+            ControllerActionDescriptor? actionDescriptor)
         {
             IsValid = isValid;
             ActionDescriptor = actionDescriptor;
         }
+
+        public static MethodCacheEntry Valid(
+            ControllerActionDescriptor actionDescriptor)
+        {
+            return new(
+                isValid: true,
+                actionDescriptor);
+        }
+
+        public static MethodCacheEntry Invalid() => InvalidInstance;
     }
 }
